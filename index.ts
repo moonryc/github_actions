01fd031c1347647551dd255339c3fa317e9b0ff0
Ryan Moon <ryanmoondevelopment@gmail.com>
2023-06-25 11:48:10 -0500

    others

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { addFn } from "./lib"
+
+
+const numberOne = 242354235
+const numberTwo = 21341234
+
+const sumOfAboveNumbers = addFn(numberOne, numberTwo)
+
+console.log(`The sum of ${numberOne} and ${numberTwo} is: ${sumOfAboveNumbers}`)
---
+++ lib/index.ts
@@ -0,0 +1,1 @@
+export { default as addFn } from "./addFn"
