e40a39a4137a15797db43f5f64797ccf01d0bd8b
Ryan Moon <ryanmoondevelopment@gmail.com>
2023-06-25 13:40:16 -0500

    help

--- index.ts
+++ index.ts
@@ -6,6 +6,6 @@ const numberTwo = 21341234
 
 const sumOfAboveNumbers = addFn(numberOne, numberTwo)
 const differenceOfAboveNumbers = subtractFn(numberOne, numberTwo)
-console.log("hello")
+console.log("help")
 console.log(`The sum of ${numberOne} and ${numberTwo} is: ${sumOfAboveNumbers}`)
 console.log(`The difference of ${numberOne} and ${numberTwo} is: ${differenceOfAboveNumbers}`)
